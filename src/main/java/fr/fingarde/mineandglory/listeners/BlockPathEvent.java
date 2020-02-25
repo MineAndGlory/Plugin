539e6688623e82227bfe3d7a6b453d14abeb9fc8
BusBaguette <jototo750@gmail.com>
2020-02-25 10:49:28 +0100

    ADDED THE BLOCK OF EVENT

--- src/main/java/fr/fingarde/mineandglory/listeners/BlockPathEvent.java
+++ src/main/java/fr/fingarde/mineandglory/listeners/BlockPathEvent.java
@@ -5,13 +5,15 @@ import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class BlockPathEvent implements Listener {
 
     @EventHandler
     public void onMove(PlayerMoveEvent event) {
         if (event.getPlayer().getLocation().getBlock().getType() == Material.GRASS_PATH) {
-            Bukkit.broadcastMessage("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaz");
+            event.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 1, 1,true,false ,false));
         }
     }
 
