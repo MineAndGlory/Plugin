65e8d03fc1e960b94f5e1f4be131c8cdbd687473
Fingarde <fingarde@gmail.com>
2020-02-26 11:27:48 +0100

    Working on Title

--- src/main/java/fr/fingarde/mineandglory/utils/Title.java
+++ src/main/java/fr/fingarde/mineandglory/utils/Title.java
@@ -9,6 +9,15 @@ public class Title {
     }
 
     public static void sendTitle(Player player, String message) {
-        Bukkit.dispatchCommand(new NoLogSender(), "title " + player.getName() + " title {\"text\":\"" + message + "\"}");
+        sendTitle(player, message, 40);
+    }
+
+    public static void sendTitle(Player player, String message, int duration) {
+        sendTitle(player, message, 5, duration, 5);
+    }
+
+    public static void sendTitle(Player player, String message, int fadeIn, int duration, int fadeOut) {
+        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "title " + player.getName() + " times " + fadeIn + " " + duration + " " + fadeOut);
+        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "title " + player.getName() + " title {\"text\":\"" + message + "\"}");
     }
 }
