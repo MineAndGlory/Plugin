0aa54cf34a30501d75ddf60ca5185a18e7f8cff1
Fingarde <fingarde@gmail.com>
2020-04-11 14:37:34 +0200

    Added ColorUtils class

---
+++ src/main/java/fr/fingarde/mineandglory/utils/ColorUtils.java
@@ -0,0 +1,12 @@
+package fr.fingarde.mineandglory.utils;
+
+public class ColorUtils
+{
+    public static String removeColor(String str) {
+        return str.replaceAll("§*", "");
+    }
+
+    public static String encodeAmperstamp(String str) {
+        return str.replaceAll("&", "§");
+    }
+}
