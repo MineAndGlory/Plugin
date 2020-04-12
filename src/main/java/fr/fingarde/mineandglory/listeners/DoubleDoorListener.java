aa60e8251f4623dcd908a4318af9588a3e0e54b0
Fingarde <fingarde@gmail.com>
2020-04-12 13:50:45 +0200

    Working on doors

--- src/main/java/fr/fingarde/mineandglory/listeners/DoubleDoorListener.java
+++ src/main/java/fr/fingarde/mineandglory/listeners/DoubleDoorListener.java
@@ -1,5 +1,6 @@
 package fr.fingarde.mineandglory.listeners;
 
+import org.bukkit.Bukkit;
 import org.bukkit.block.data.type.Door;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -11,31 +12,42 @@ public class DoubleDoorListener implements Listener
     public void onDoorClick(PlayerInteractEvent event)
     {
         if(event.getClickedBlock() == null) return;
+        Bukkit.broadcastMessage("1");
         if(!event.getClickedBlock().getType().toString().endsWith("_DOOR")) return;
+        Bukkit.broadcastMessage("2");
 
         Door door = (Door) event.getClickedBlock().getBlockData();
         Door relative;
+
         if(event.getClickedBlock().getRelative(1, 0, 0).getType() == event.getClickedBlock().getType()) {
+            Bukkit.broadcastMessage("3");
             relative = (Door) event.getClickedBlock().getRelative(1, 0, 0).getBlockData();
             if(relative.getHinge() != getInverse(door.getHinge())) {
+                Bukkit.broadcastMessage("7");
                 relative.setOpen(door.isOpen());
             }
         }
         if(event.getClickedBlock().getRelative(-1, 0, 0).getType() == event.getClickedBlock().getType()) {
+            Bukkit.broadcastMessage("4");
             relative = (Door) event.getClickedBlock().getRelative(-1, 0, 0).getBlockData();
             if(relative.getHinge() != getInverse(door.getHinge())) {
+                Bukkit.broadcastMessage("8");
                 relative.setOpen(door.isOpen());
             }
         }
         if(event.getClickedBlock().getRelative(0, 0, 1).getType() == event.getClickedBlock().getType()) {
+            Bukkit.broadcastMessage("5");
             relative = (Door) event.getClickedBlock().getRelative(0, 0, 1).getBlockData();
             if(relative.getHinge() != getInverse(door.getHinge())) {
+                Bukkit.broadcastMessage("9");
                 relative.setOpen(door.isOpen());
             }
         }
         if(event.getClickedBlock().getRelative(0, 0, -1).getType() == event.getClickedBlock().getType()) {
+            Bukkit.broadcastMessage("6");
             relative = (Door) event.getClickedBlock().getRelative(0, 0, -1).getBlockData();
             if(relative.getHinge() != getInverse(door.getHinge())) {
+                Bukkit.broadcastMessage("10");
                 relative.setOpen(door.isOpen());
             }
         }
