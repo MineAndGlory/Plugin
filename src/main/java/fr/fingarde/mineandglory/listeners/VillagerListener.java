db57ba5c5b37af0dc1b7d730e2cc2ba05dfb39d0
Fingarde <fingarde@gmail.com>
2020-04-14 15:17:48 +0200

    Door fix

--- src/main/java/fr/fingarde/mineandglory/listeners/VillagerListener.java
+++ src/main/java/fr/fingarde/mineandglory/listeners/VillagerListener.java
@@ -4,10 +4,12 @@ import fr.fingarde.mineandglory.Main;
 import fr.fingarde.mineandglory.objects.items.CustomItems;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Villager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.VillagerCareerChangeEvent;
+import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.MerchantRecipe;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -18,24 +20,15 @@ import java.util.List;
 public class VillagerListener implements Listener
 {
     @EventHandler
-    public void onVillagerSpawn(VillagerCareerChangeEvent event) {
+    public void onVillagerSpawn(PlayerInteractAtEntityEvent event) {
+        if(event.getRightClicked().getType() != EntityType.VILLAGER) return;
 
+        Villager villager = (Villager) event.getRightClicked();
+        if(villager.getProfession() == Villager.Profession.NONE) return;
 
-        new BukkitRunnable()
-        {
-            @Override
-            public void run()
-            {
-                Villager villager = event.getEntity();
-                Bukkit.broadcastMessage("ouioiuo");
+        List<MerchantRecipe> recipes = villager.getRecipes();
+        recipes.forEach(merchantRecipe -> merchantRecipe.getResult().setType(Material.STONE));
 
-                ItemStack coin = CustomItems.getFromValue(CustomItems.VILLAGER_COIN);
-                List<MerchantRecipe> recipes = villager.getRecipes();
-                recipes.forEach(merchantRecipe -> merchantRecipe.getResult().setType(coin.getType()));
-                villager.setRecipes(recipes);
-
-            }
-        }.runTaskLater(Main.getPlugin(), 100);
 
     }
 }
