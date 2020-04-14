6b2c41fb6f1e97f497b5923dfe8f706386831e75
Fingarde <fingarde@gmail.com>
2020-04-14 15:45:56 +0200

    Working on villagers

--- src/main/java/fr/fingarde/mineandglory/listeners/VillagerListener.java
+++ src/main/java/fr/fingarde/mineandglory/listeners/VillagerListener.java
@@ -1,5 +1,6 @@
 package fr.fingarde.mineandglory.listeners;
 
+import fr.fingarde.mineandglory.Main;
 import fr.fingarde.mineandglory.objects.items.CustomItems;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
@@ -7,40 +8,50 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Villager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.VillagerCareerChangeEvent;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.MerchantRecipe;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.List;
 
 public class VillagerListener implements Listener
 {
     @EventHandler
-    public void onVillagerSpawn(PlayerInteractAtEntityEvent event) {
-        if(event.getRightClicked().getType() != EntityType.VILLAGER) return;
-
-        Villager villager = (Villager) event.getRightClicked();
-        if(villager.getProfession() == Villager.Profession.NONE) return;
-
-        List<MerchantRecipe> recipes = villager.getRecipes();
-        Bukkit.broadcastMessage(recipes.size() + "");
-        ItemStack coin = CustomItems.getFromValue(CustomItems.VILLAGER_COIN);
-
-        for(MerchantRecipe recipe : recipes)
+    public void onVillagerSpawn(VillagerCareerChangeEvent event) {
+        new BukkitRunnable()
         {
-            ItemStack stack = recipe.getResult();
-            stack.setAmount(10);
-            if(stack.getType() == Material.EMERALD) {
+            @Override
+            public void run()
+            {
+                //if(event.getEntity().getType() != EntityType.VILLAGER) return;
 
-                stack.setType(coin.getType());
-                stack.setItemMeta(coin.getItemMeta());
+                Villager villager = (Villager) event.getEntity();
+                if(villager.getProfession() == Villager.Profession.NONE) return;
 
+                List<MerchantRecipe> recipes = villager.getRecipes();
+                Bukkit.broadcastMessage(recipes.size() + "");
+                ItemStack coin = CustomItems.getFromValue(CustomItems.VILLAGER_COIN);
+
+                for(MerchantRecipe recipe : recipes)
+                {
+                    ItemStack stack = recipe.getResult();
+                    stack.setAmount(10);
+                    if(stack.getType() == Material.EMERALD) {
+
+                        stack.setType(coin.getType());
+                        stack.setItemMeta(coin.getItemMeta());
+
+                    }
+
+                    MerchantRecipe recipe1 = new MerchantRecipe(stack, recipe.getMaxUses());
+                    recipe1.setIngredients(recipe.getIngredients());
+                    villager.setRecipe(recipes.indexOf(recipe), recipe1);
+                }
             }
+        }.runTaskLater(Main.getPlugin(), 10);
 
-            MerchantRecipe recipe1 = new MerchantRecipe(stack, recipe.getMaxUses());
-            recipe1.setIngredients(recipe.getIngredients());
-            villager.setRecipe(recipes.indexOf(recipe), recipe1);
-        }
        // recipes.forEach(merchantRecipe -> );
        // villager.setRecipes(recipes);
     }
