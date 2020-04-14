004774148cdcbb0c518399edad09097232acf76d
Fingarde <fingarde@gmail.com>
2020-04-14 16:49:41 +0200

    Working on spawner pickaxe

--- src/main/java/fr/fingarde/mineandglory/listeners/items/SpawnerListener.java
+++ src/main/java/fr/fingarde/mineandglory/listeners/items/SpawnerListener.java
@@ -2,9 +2,12 @@ package fr.fingarde.mineandglory.listeners.items;
 
 import fr.fingarde.mineandglory.objects.items.CustomItems;
 import org.bukkit.Material;
+import org.bukkit.block.CreatureSpawner;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class SpawnerListener implements Listener
 {
@@ -12,7 +15,14 @@ public class SpawnerListener implements Listener
     public void onBreak(BlockBreakEvent event) {
         if(event.getPlayer().getInventory().getItemInMainHand().getType() != Material.DIAMOND_PICKAXE) return;
         if(!event.getPlayer().getInventory().getItemInMainHand().getItemMeta().getLocalizedName().equals(CustomItems.SPAWNER_PICKAXE.name())) return;
+        if(event.getBlock().getType() != Material.SPAWNER) return;
 
-        event.setCancelled(true);
+        ItemStack spawner = new ItemStack(Material.SPAWNER);
+        ItemMeta meta = spawner.getItemMeta();
+        meta.setDisplayName(((CreatureSpawner) event.getBlock().getState()).getSpawnedType().name() + " SPAWNER");
+        spawner.setItemMeta(meta);
+
+        event.getBlock().getWorld().dropItem(event.getBlock().getLocation(), spawner);
+
     }
 }
