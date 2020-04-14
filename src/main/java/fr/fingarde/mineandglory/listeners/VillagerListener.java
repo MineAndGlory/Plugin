2a1563835d7ab644c0f5f983cddc2d01f2b909dc
Fingarde <fingarde@gmail.com>
2020-04-14 15:21:25 +0200

    Working on villagers

--- src/main/java/fr/fingarde/mineandglory/listeners/VillagerListener.java
+++ src/main/java/fr/fingarde/mineandglory/listeners/VillagerListener.java
@@ -1,20 +1,14 @@
 package fr.fingarde.mineandglory.listeners;
 
-import fr.fingarde.mineandglory.Main;
-import fr.fingarde.mineandglory.objects.items.CustomItems;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Villager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.VillagerCareerChangeEvent;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.MerchantRecipe;
-import org.bukkit.scheduler.BukkitRunnable;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class VillagerListener implements Listener
@@ -27,6 +21,7 @@ public class VillagerListener implements Listener
         if(villager.getProfession() == Villager.Profession.NONE) return;
 
         List<MerchantRecipe> recipes = villager.getRecipes();
+        Bukkit.broadcastMessage(recipes.size() + "");
         recipes.forEach(merchantRecipe -> merchantRecipe.getResult().setType(Material.STONE));
 
         villager.setRecipes(recipes);
