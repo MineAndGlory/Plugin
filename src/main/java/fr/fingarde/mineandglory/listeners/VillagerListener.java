abab018001ea8a2d4337fb575aa384afc6f5381c
Fingarde <fingarde@gmail.com>
2020-04-14 14:52:47 +0200

    Working

--- src/main/java/fr/fingarde/mineandglory/listeners/VillagerListener.java
+++ src/main/java/fr/fingarde/mineandglory/listeners/VillagerListener.java
@@ -1,6 +1,7 @@
 package fr.fingarde.mineandglory.listeners;
 
 import fr.fingarde.mineandglory.objects.items.CustomItems;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Villager;
 import org.bukkit.event.EventHandler;
@@ -8,6 +9,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.VillagerCareerChangeEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.MerchantRecipe;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.List;
 
@@ -15,14 +17,16 @@ public class VillagerListener implements Listener
 {
     @EventHandler
     public void onVillagerSpawn(VillagerCareerChangeEvent event) {
+        Bukkit.broadcastMessage("ouioiuo");
         Villager villager = event.getEntity();
         ItemStack coin = CustomItems.getFromValue(CustomItems.VILLAGER_COIN);
 
         List<MerchantRecipe> recipes = villager.getRecipes();
         recipes.forEach(merchantRecipe -> {
-            if(merchantRecipe.getResult().getType() == Material.EMERALD)
+            merchantRecipe.getResult().setType(Material.STONE);
+          /*  if(merchantRecipe.getResult().getType() == Material.EMERALD)
                 merchantRecipe.getResult().setType(coin.getType());
-                merchantRecipe.getResult().setItemMeta(coin.getItemMeta());
+                merchantRecipe.getResult().setItemMeta(coin.getItemMeta());*/
         });
 
         villager.setRecipes(recipes);
