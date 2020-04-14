87693f8ee27bf697935fb3f4ccb44caed17d4b4b
Fingarde <fingarde@gmail.com>
2020-04-14 22:23:54 +0200

    Motd

--- src/main/java/fr/fingarde/mineandglory/listeners/ServerListListener.java
+++ src/main/java/fr/fingarde/mineandglory/listeners/ServerListListener.java
@@ -1,16 +1,14 @@
 package fr.fingarde.mineandglory.listeners;
 
-import org.bukkit.Bukkit;
+import fr.fingarde.mineandglory.utils.managers.TimeManager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.server.ServerListPingEvent;
-
-import java.util.Iterator;
 
 public class ServerListListener implements Listener
 {
     @EventHandler
     public void onPing(ServerListPingEvent event) {
-        event.setMotd("§6§m               §r§7[ §r§eMineAndGlory  §7]§6§m        §r§7[  20h12  §7]§6§m        \n§c§l≫ En maintenance");
+        event.setMotd("§6§m               §r§7[ §r§eMineAndGlory  §7]§6§m        §r§7[  " + TimeManager.getHeure()  + "  §7]§6§m        \n       §c§l≫ En maintenance");
     }
 }
