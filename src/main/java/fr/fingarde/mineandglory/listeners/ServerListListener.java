06149ada3e8adbad0d228ac0b310d005aa5205ba
Fingarde <fingarde@gmail.com>
2020-04-14 22:19:42 +0200

    Motd

--- src/main/java/fr/fingarde/mineandglory/listeners/ServerListListener.java
+++ src/main/java/fr/fingarde/mineandglory/listeners/ServerListListener.java
@@ -11,7 +11,6 @@ public class ServerListListener implements Listener
 {
     @EventHandler
     public void onPing(ServerListPingEvent event) {
-        event.setMotd("123456789012345678901234567890123456789012345\n" +
-                "§6§m               §r§7[ §r§eMineAndGlory  §7]§6§m        §r§7[  20h12  §7]§6§m          ");
+        event.setMotd("§6§m               §r§7[ §r§eMineAndGlory  §7]§6§m        §r§7[  20h12  §7]§6§m          ");
     }
 }
