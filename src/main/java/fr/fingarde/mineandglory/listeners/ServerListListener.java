7552f6e789b9c8fa393618310bda4e7371e3e9b4
Fingarde <fingarde@gmail.com>
2020-04-14 21:55:53 +0200

    Motd

--- src/main/java/fr/fingarde/mineandglory/listeners/ServerListListener.java
+++ src/main/java/fr/fingarde/mineandglory/listeners/ServerListListener.java
@@ -11,6 +11,6 @@ public class ServerListListener implements Listener
 {
     @EventHandler
     public void onPing(ServerListPingEvent event) {
-        event.setMotd("&e&l&m--&9&8&l[&r &6&lThe Hypixel Network &8&l]&e&l&m--&8&l[ &eSub Line &8&l]&e&l&m-----&e&l&m ---&8&l[ &eNEWS:&8&l ]&e&m----&8&l[&r &e         &l HEADLINE           &8&l]&e&l&m-");
+        event.setMotd("§e§l§m--§9§8§l[§r §6§lThe Hypixel Network §8§l]§e§l§m--§8§l[ §eSub Line §8§l]§e§l§m-----§e§l§m ---§8§l[ §eNEWS:§8§l ]§e§m----§8§l[§r §e         §l HEADLINE           §8§l]§e§l§m-");
     }
 }
