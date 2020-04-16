7a8eb25d096d8d117bfe0d532a3e18a2e2e340e5
Fingarde <fingarde@gmail.com>
2020-04-16 18:31:58 +0200

    Working on commands

--- src/main/java/fr/fingarde/mineandglory/commands/GodCommand.java
+++ src/main/java/fr/fingarde/mineandglory/commands/GodCommand.java
@@ -2,7 +2,6 @@ package fr.fingarde.mineandglory.commands;
 
 import fr.fingarde.mineandglory.utils.ErrorMessage;
 import org.bukkit.Bukkit;
-import org.bukkit.attribute.Attribute;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
