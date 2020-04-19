ab834d2f40eebdf3d4f665ae1c156238eb3dcbb8
Fingarde <fingarde@gmail.com>
2020-04-19 19:47:31 +0200

    Working on crops

--- src/main/java/fr/fingarde/mineandglory/listeners/blocks/CropsListener.java
+++ src/main/java/fr/fingarde/mineandglory/listeners/blocks/CropsListener.java
@@ -1,10 +1,17 @@
 package fr.fingarde.mineandglory.listeners.blocks;
 
+import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.block.data.Ageable;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockGrowEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.loot.LootContext;
+
+import java.util.Collection;
+import java.util.Random;
 
 public class CropsListener implements Listener
 {
@@ -14,9 +21,9 @@ public class CropsListener implements Listener
         Block block = event.getBlock();
         Ageable ageable = (Ageable) block.getBlockData();
 
-        // need crops
-        event.setCancelled(true);
+        if (!isCrop(block.getType())) return;
 
+        event.setCancelled(true);
         if (ageable.getAge() + 1 == ageable.getMaximumAge())
         {
             return;
@@ -25,5 +32,31 @@ public class CropsListener implements Listener
         ageable.setAge(ageable.getAge() + 2);
 
         block.setBlockData(ageable);
+    }
+
+    @EventHandler
+    public void onClick(PlayerInteractEvent event)
+    {
+        Collection<ItemStack> items = Bukkit.getLootTable(NamespacedKey.minecraft("blocks/lettuce")).populateLoot(new Random(), new LootContext.Builder(new Location(Bukkit.getWorld("world"), 5, 5, 5))
+                .build());
+
+        items.forEach(itemStack -> event.getPlayer().getWorld().dropItem(event.getPlayer().getLocation(), itemStack));
+
+    }
+
+    private boolean isCrop(Material material)
+    {
+
+
+        switch (material)
+        {
+            case WHEAT:
+            case POTATOES:
+            case CARROTS:
+            case BEETROOTS:
+                return true;
+        }
+
+        return false;
     }
 }
